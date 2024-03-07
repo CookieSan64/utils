d8c264d9d0a1f2ebf0ae326ebcad2c584f9db2c1
ChouCookieSan <98222012+CookieSan64@users.noreply.github.com>
2024-03-07 17:16:53 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,337 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+-- Get the ESX library --
+ESX = exports["es_extended"]:getSharedObject()
+
+------- Pause Menu texte 
+function SetData()
+	players = {}
+	for _, player in ipairs(GetActivePlayers()) do
+		local ped = GetPlayerPed(player)
+		table.insert( players, player )
+end
+	local name = GetPlayerName(PlayerId())
+	local id = GetPlayerServerId(PlayerId())
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', '~r~Calico City~s~ | ID : ~p~'.. id ..'~w~')
+end
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(100)
+		SetData()
+	end
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("PM_PANE_LEAVE", "~r~Se déconnecter ~w~de~r~ Calico City")
+end)
+
+Citizen.CreateThread(function()
+    AddTextEntry("PM_PANE_QUIT", "Quitter ~y~FiveM 🐌")
+end)
+
+-- PiggyBack
+local carry = {
+	InProgress = false,
+	targetSrc = -1,
+	type = "",
+	personCarrying = {
+		animDict = "missfinale_c2mcs_1",
+		anim = "fin_c2_mcs_1_camman",
+		flag = 49,
+	},
+	personCarried = {
+		animDict = "nm",
+		anim = "firemans_carry",
+		attachX = 0.27,
+		attachY = 0.15,
+		attachZ = 0.63,
+		flag = 33,
+	}
+}
+
+local function drawNativeNotification(text)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(text)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+local function GetClosestPlayer(radius)
+    local players = GetActivePlayers()
+    local closestDistance = -1
+    local closestPlayer = -1
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+
+    for _,playerId in ipairs(players) do
+        local targetPed = GetPlayerPed(playerId)
+        if targetPed ~= playerPed then
+            local targetCoords = GetEntityCoords(targetPed)
+            local distance = #(targetCoords-playerCoords)
+            if closestDistance == -1 or closestDistance > distance then
+                closestPlayer = playerId
+                closestDistance = distance
+            end
+        end
+    end
+	if closestDistance ~= -1 and closestDistance <= radius then
+		return closestPlayer
+	else
+		return nil
+	end
+end
+
+local function ensureAnimDict(animDict)
+    if not HasAnimDictLoaded(animDict) then
+        RequestAnimDict(animDict)
+        while not HasAnimDictLoaded(animDict) do
+            Wait(0)
+        end        
+    end
+    return animDict
+end
+
+RegisterCommand("porter",function(source, args)
+	if not carry.InProgress then
+		local closestPlayer = GetClosestPlayer(3)
+		if closestPlayer then
+			local targetSrc = GetPlayerServerId(closestPlayer)
+			if targetSrc ~= -1 then
+				carry.InProgress = true
+				carry.targetSrc = targetSrc
+				TriggerServerEvent("CarryPeople:sync",targetSrc)
+				ensureAnimDict(carry.personCarrying.animDict)
+				carry.type = "carrying"
+			else
+				drawNativeNotification("~r~No one nearby to carry!")
+			end
+		else
+			drawNativeNotification("~r~No one nearby to carry!")
+		end
+	else
+		carry.InProgress = false
+		ClearPedSecondaryTask(PlayerPedId())
+		DetachEntity(PlayerPedId(), true, false)
+		TriggerServerEvent("CarryPeople:stop",carry.targetSrc)
+		carry.targetSrc = 0
+	end
+end,false)
+
+RegisterNetEvent("CarryPeople:syncTarget")
+AddEventHandler("CarryPeople:syncTarget", function(targetSrc)
+	local targetPed = GetPlayerPed(GetPlayerFromServerId(targetSrc))
+	carry.InProgress = true
+	ensureAnimDict(carry.personCarried.animDict)
+	AttachEntityToEntity(PlayerPedId(), targetPed, 0, carry.personCarried.attachX, carry.personCarried.attachY, carry.personCarried.attachZ, 0.5, 0.5, 180, false, false, false, false, 2, false)
+	carry.type = "beingcarried"
+end)
+
+RegisterNetEvent("CarryPeople:cl_stop")
+AddEventHandler("CarryPeople:cl_stop", function()
+	carry.InProgress = false
+	ClearPedSecondaryTask(PlayerPedId())
+	DetachEntity(PlayerPedId(), true, false)
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		if carry.InProgress then
+			if carry.type == "beingcarried" then
+				if not IsEntityPlayingAnim(PlayerPedId(), carry.personCarried.animDict, carry.personCarried.anim, 3) then
+					TaskPlayAnim(PlayerPedId(), carry.personCarried.animDict, carry.personCarried.anim, 8.0, -8.0, 100000, carry.personCarried.flag, 0, false, false, false)
+				end
+			elseif carry.type == "carrying" then
+				if not IsEntityPlayingAnim(PlayerPedId(), carry.personCarrying.animDict, carry.personCarrying.anim, 3) then
+					TaskPlayAnim(PlayerPedId(), carry.personCarrying.animDict, carry.personCarrying.anim, 8.0, -8.0, 100000, carry.personCarrying.flag, 0, false, false, false)
+				end
+			end
+		end
+		Wait(0)
+	end
+end)
+
+--- Put the train
+Citizen.CreateThread(function()
+    SwitchTrainTrack(0, true)
+    SwitchTrainTrack(3, true)
+    N_0x21973bbf8d17edfa(0, 120000)
+    SetRandomTrains(true)
+end)
+
+-- DISABLE AUTO PLACE CHANGE
+local disableShuffle = true
+function disableSeatShuffle(flag)
+	disableShuffle = flag
+end
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if IsPedInAnyVehicle(GetPlayerPed(-1), false) and disableShuffle then
+			if GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), 0) == GetPlayerPed(-1) then
+				if GetIsTaskActive(GetPlayerPed(-1), 165) then
+					SetPedIntoVehicle(GetPlayerPed(-1), GetVehiclePedIsIn(GetPlayerPed(-1), false), 0)
+				end
+			end
+		end
+	end
+end)
+
+-- KEYBIND CHANGE PLACE VEHICLE
+Citizen.CreateThread(function()
+    while true do
+        local plyPed = PlayerPedId()
+        if IsPedSittingInAnyVehicle(plyPed) then
+            local plyVehicle = GetVehiclePedIsIn(plyPed, false)
+			CarSpeed = GetEntitySpeed(plyVehicle) * 3.6 -- We define the speed of the vehicle in km/h
+			if CarSpeed <= 40.0 then -- You cannot change places if the speed of the vehicle is above or equal to 60 km/h
+				if IsControlJustReleased(0, 27) then -- driver --Click Molette
+					SetPedIntoVehicle(plyPed, plyVehicle, -1)
+					Citizen.Wait(10)
+				end
+				if IsControlJustReleased(0, 50) then -- Before right --Molette vers le bas
+					SetPedIntoVehicle(plyPed, plyVehicle, 0)
+					Citizen.Wait(10)
+				end
+				if IsControlJustReleased(0, 99) then -- back left --Molette vers le haut
+					SetPedIntoVehicle(plyPed, plyVehicle, 1)
+					Citizen.Wait(10)
+				end
+				if IsControlJustReleased(0, 83) then -- back left -- = 
+					SetPedIntoVehicle(plyPed, plyVehicle, 2)
+					Citizen.Wait(10)
+				end
+			end
+		end
+		Citizen.Wait(10) -- Fix Crash Client
+	end
+end)
+
+--[[---------- disable pnj Carjacking
+ Citizen.CreateThread(function()
+     while true do
+         Wait(800)
+         local player = GetPlayerPed(-1)
+         local PlayerPedId = PlayerPedId(player)
+         local veh = GetVehiclePedIsTryingToEnter(PlayerPedId)
+         if veh ~= nil and DoesEntityExist(veh) then
+             local lockStatus = GetVehicleDoorLockStatus(veh)
+             if lockStatus == 7 then
+                 SetVehicleDoorsLocked(veh, 2)
+             end
+             local ped = GetPedInVehicleSeat(veh, -1)
+             if ped then
+                 SetPedCanBeDraggedOut(ped, false)
+             end
+         end
+     end
+ end)]]
+
+ -- Coup de cross 
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(100)
+	 local ped = PlayerPedId()
+	  if IsPedArmed(ped, 6) then
+			DisableControlAction(1, 140, true)
+			DisableControlAction(1, 141, true)
+		 DisableControlAction(1, 142, true)
+	   end
+	end
+ end)
+ Citizen.CreateThread(function()
+	 while true do
+		 Citizen.Wait(5) 
+		 if (not IsPedArmed(PlayerPedId(), 1)) and (GetSelectedPedWeapon(PlayerPedId()) ~= GetHashKey('weapon_unarmed')) then
+			 DisableControlAction(0, 140, true)
+			 DisableControlAction(0, 141, true)
+			 DisableControlAction(0, 142, true)
+		 end
+	 end
+ end)
+ ESX = exports["es_extended"]:getSharedObject()
+ local player = GetPlayerPed(-1)
+ local coords = GetEntityCoords(player)
+
+  --------------- NoNPCEmergency
+function DisableDispatch()
+    for a = 1, 15 do
+        EnableDispatchService(a, false)
+    end
+end
+
+function RemoveAudio()
+    DisablePoliceReports()
+end
+
+function RemoveWantedLevel()
+    if GetPlayerWantedLevel(PlayerId()) >= 1 then
+        SetPlayerWantedLevel(PlayerId(), 0, false)
+        SetPlayerWantedLevelNow(PlayerId(), false)
+    end
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(0)
+        for i = 1, 15 do
+            EnableDispatchService(i, false)
+        end
+        SetPlayerWantedLevel(PlayerId(), 0, false)
+        SetPlayerWantedLevelNow(PlayerId(), false)
+        SetPlayerWantedLevelNoDrop(PlayerId(), 0, false)
+    end
+end)
+
+ --------------- KNOCKOUT
+local knockedOut = false
+local wait = 15
+local count = 60
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(5)
+		local myPed = GetPlayerPed(-1)
+		if IsPedInMeleeCombat(myPed) then
+			if GetEntityHealth(myPed) < 115 then
+				SetPlayerInvincible(PlayerId(), true)
+				SetPedToRagdoll(myPed, 1000, 1000, 0, 0, 0, 0)
+				ShowNotification("~r~Vous êtes K.O")
+				wait = 15
+				knockedOut = true
+				SetEntityHealth(myPed, 116)
+			end
+		end
+		if knockedOut == true then
+			SetPlayerInvincible(PlayerId(), true)
+			DisablePlayerFiring(PlayerId(), true)
+			SetPedToRagdoll(myPed, 1000, 1000, 0, 0, 0, 0)
+			ResetPedRagdollTimer(myPed)
+			if wait >= 0 then
+				count = count - 1
+				if count == 0 then
+					count = 60
+					wait = wait - 1
+					SetEntityHealth(myPed, GetEntityHealth(myPed)+4)
+				end
+			else
+				SetPlayerInvincible(PlayerId(), false)
+				knockedOut = false
+			end
+		end
+	end
+end)
+
+function ShowNotification(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(false, false)
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+client_scripts {
+	'client.lua',
+    '@bulletin/client.lua'
+} 
+server_scripts {
+    'server.lua'
+}
+shared_script '@es_extended/imports.lua'
